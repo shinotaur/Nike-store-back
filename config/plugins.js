a54ef6ea17ce30f7918bbe9f4c63538f7e0363c7
shinotaur <71105630+shinotaur@users.noreply.github.com>
2024-02-13 20:03:41 +0530

    updated

--- config/plugins.js
+++ config/plugins.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
       apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
       appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      roles: ["strapi-super-admin"],
     },
   },
   upload: {
